b545db4d46c6d510ba00a08d75555ca4617f08d3
Redha Zidan <108479734+zidanredha2@users.noreply.github.com>
2025-06-05 00:58:53 +0530

    Delete helpdesk directory

--- helpdesk/helpdesk/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-from myapp import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
-]
